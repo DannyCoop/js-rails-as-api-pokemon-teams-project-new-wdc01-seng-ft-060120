6cfbe767ae93b12cfa7263d92cc249ec90baa03f
DannyCoop <cooperdaniel5141@gmail.com>
2020-07-20 16:15:41 -0400

    finished the back end and started the front end

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,52 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+// When a user loads the page, they should see all trainers, 
+// with their current team of Pokemon.
+
+document.addEventListener("DOMContentLoaded", () => {
+    const getTrainers = () => {
+        fetch(TRAINERS_URL)
+        .then(res => res.json())
+        .then(listOfTrainers => {renderCards(listOfTrainers)})
+    }
+
+    
+    const renderCards = (listOfCards) => {
+        listOfCards.forEach(trainer => renderCard(trainer) );
+    }
+
+    const renderCard = (trainer) => {
+        let main = document.querySelector('main')
+        
+        let div = document.createElement('div')
+        div.className = 'card'
+        div.dataset.id = trainer.id
+        main.appendChild(div)
+
+        let p = document.createElement('p')
+        p.innerText = trainer.name;
+        div.appendChild(p)
+        
+        let button = document.createElement('button')
+        button.dataset.trainerId = trainer.id
+        button.innerText = 'Add Pokemon'
+        div.appendChild(button)
+
+        let teamList = document.createElement('ul')
+        div.appendChild(teamList)
+
+        // for(const name in trainer.pokemons) {
+        //     console.log(`${name}: ${trainer.pokemons[name]}`)
+        // }
+
+        console.log(trainer)
+
+
+
+    }
+
+    getTrainers()
+})
+
